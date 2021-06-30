feff1bb040274134bd8c3650634c371e92bd6958
Sihan-Tao <sihan.tao20@imperial.ac.uk>
2021-06-30 10:47:22 +0800

    763.Partition Labels solution 2

--- src/PartitionLabels_763.java
+++ src/PartitionLabels_763.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public class PartitionLabels_763 {
-  public List<Integer> partitionLabels(String s) {
+  public List<Integer> partitionLabels_s1(String s) {
     Map<Character, List<Integer>> map = new HashMap<>();
     int n = s.length();
     for (int i = 0; i < n; i++) {
@@ -37,5 +37,23 @@ public class PartitionLabels_763 {
     result.add(end - start + 1);
 
     return result;
+  }
+
+  public List<Integer> partitionLabels(String s) {
+    int[] last = new int[26];
+    int length = s.length();
+    for (int i = 0; i < length; i++) {
+      last[s.charAt(i) - 'a'] = i;
+    }
+    List<Integer> partition = new ArrayList<>();
+    int start = 0, end = 0;
+    for (int i = 0; i < length; i++) {
+      end = Math.max(end, last[s.charAt(i) - 'a']);
+      if (i == end) {
+        partition.add(end - start + 1);
+        start = end + 1;
+      }
+    }
+    return partition;
   }
 }
