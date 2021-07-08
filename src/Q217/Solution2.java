f84fcfc7e59b6537edfe1f2930c2d1dd332d3643
Sihan-Tao <sihan.tao20@imperial.ac.uk>
2021-07-08 16:42:49 +0800

    Q217: O(nlogn)

---
+++ src/Q217/Solution2.java
@@ -0,0 +1,19 @@
+package Q217;
+
+import java.util.Arrays;
+
+/**
+ * O(nlogn)
+ */
+public class Solution2 {
+    public boolean containsDuplicate(int[] nums) {
+      Arrays.sort(nums);
+      int n = nums.length;
+      for (int i = 0; i < n - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+          return true;
+        }
+      }
+      return false;
+    }
+}
